e3eba9a1985eec66c8500c270733a9057aa6f5a6
ANtonylpz7 <lopezantony6400@gmail.com>
2025-04-02 17:09:42 +0200

    FormulairedeConnexion

--- controleurs/controleurUtilisateur.php
+++ controleurs/controleurUtilisateur.php
@@ -5,19 +5,15 @@ require_once("./modeles/DAO/UtilisateurDAO.php");
 if (isset($_GET['action'])){
     $action=filter_var($_GET['action'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 }else {
-$action= "consultationUtilisateurs";}
+$action= "consultation";}
 
 switch ($action){
-    case 'consultationUtilisateurs'    :
+    case 'consultation'    :
                         if(isset($_SESSION['utilisateurConnecte'])){ 
                           $utilisateurConnecte = unserialize($_SESSION['utilisateurConnecte']);
-                          if($utilisateurConnecte->getRole() == "admin"){
-                            require_once("./vues/v_utilisateursGestion.php");
-                          }else{
-                            require_once("./vues/v_utilisateurs.php");
-                          }
+                          require_once("./vues/v_profile.php");
                         }else{
-                            require_once("./vues/v_utilisateurs.php");
+                            require_once("./vues/formulaires/v_formulaireConnexion.php");
                         }
                         break;
 }
