1f502209325f0167752483e404f1acde61abef5e
Antonylpz7 <lopezantony6400@gmail.com>
2025-04-03 13:36:44 +0200

    CorrectionFormulaire

--- vues/formulaires/v_formulaireAjoutRecette.php
+++ vues/formulaires/v_formulaireAjoutRecette.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulaire avec Image</title>
-    <link rel="stylesheet" href="../../css/formulaireAjoutRecette.css">
-</head>
 <body>
+    
     <div class="form-container">
         <h2>Ajouter une recette</h2>
         <form action="#" method="post" enctype="multipart/form-data">
@@ -33,5 +26,5 @@
             </div>
         </form>
     </div>
-</body>
-</html>
+    </body>
+
