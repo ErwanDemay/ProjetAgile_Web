d1e8e9c6198822faa79dd409d42c7fa3721191be
Loan MANCISIDOR <loan.mancisidor>
2025-04-11 10:43:39 +0200

    modif formulaire ajout recette à session (css)

--- vues/formulaires/v_formulaireAjoutRecetteASession.php
+++ vues/formulaires/v_formulaireAjoutRecetteASession.php
@@ -8,7 +8,8 @@
 
     <!-- Formulaire pour ajouter une recette à la session -->
     <form method="POST" action="index.php?controleur=sessions&action=addRecetteASession">
-        <input type="hidden" name="session_id" value="<?php echo htmlspecialchars($session->getId()); ?>">
+    <div class="input-field">    
+    <input type="hidden" name="session_id" value="<?php echo htmlspecialchars($session->getId()); ?>">
         <!-- Sélection de la recette à ajouter à la session -->
         <label for="recette_id">Sélectionner une recette :</label>
         <select name="recette_id" id="recette_id" required>
@@ -16,8 +17,10 @@
                 <option value="<?php echo $recette->getId(); ?>"><?php echo htmlspecialchars($recette->getLibelle()); ?></option>
             <?php endforeach; ?>
         </select>
-
-        <button type="submit">Ajouter la recette</button>
+    </div>
+    <div class="buttons">
+        <button type="submit" class="create-btn">Ajouter la recette</button>
+    </div>
     </form>
         <a href="index.php?controleur=sessions">Retour aux sessions</a>
     </div>
